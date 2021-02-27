5c709e0a7dff2c87b1393caa350671d36ee6193d
araethomp <angela.r.thomp@gmail.com>
2021-02-26 16:01:34 -0800

    answered question 8

--- EmployeeSQL/data/analysis.sql
+++ EmployeeSQL/data/analysis.sql
@@ -68,4 +68,15 @@ WHERE d.dept_no = 'd007' OR d.dept_no = 'd005'
 ;
 
 
---In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+--In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name)  
+FROM employees
+GROUP BY last_name
+ORDER BY last_name DESC;
+
+
+
+
+
